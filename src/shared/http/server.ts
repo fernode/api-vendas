d79b70de7dd8561e1f95157de2a99f26b67030e6
Fernando Silva <fernandosilvadj@hotmail.com>
2023-03-18 00:20:23 -0300

    feat: add appErrors class

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,6 @@
+import AppErrors from '@shared/errors/appErrors'
 import cors from 'cors'
-import express from 'express'
+import express, { Request, Response } from 'express'
 import router from './routes'
 
 const app = express()
@@ -7,5 +8,19 @@ app.use(express.json())
 app.use(cors())
 
 app.use(router)
+
+app.use((error: Error, request: Request, response: Response) => {
+	if (error instanceof AppErrors) {
+		return response.status(error.statusCode).json({
+			status: 'error',
+			message: error.message,
+		})
+	}
+
+	return response.status(500).json({
+		status: 'error',
+		message: 'Internal server error',
+	})
+})
 
 app.listen(3333, () => console.log('Server is running on port 3333 🚀'))

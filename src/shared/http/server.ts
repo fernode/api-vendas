7f7606f59b055878d70084d78f64ac36c577d15f
Fernando Silva <fernandosilvadj@hotmail.com>
2023-03-17 22:56:51 -0300

    feat: config routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+import cors from 'cors'
+import express from 'express'
+import router from './routes'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use(router)
+
+app.listen(3333, () => console.log('Server is running on port 3333 🚀'))

dff29b56a17db3345b9ec6da499f4d473669ea98
Fernando Silva <fernandosilvadj@hotmail.com>
2023-03-27 16:07:00 -0300

    encrypt user password

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,7 +1,7 @@
+import bcrypt from 'bcrypt'
 import { Request, Response } from 'express'
 import { CreateUserService } from '../typeorm/services/CreateUserService'
 import { ListUserService } from '../typeorm/services/ListUserService'
-
 class UserController {
 	public async index(req: Request, res: Response): Promise<Response> {
 		const listUserService = new ListUserService()
@@ -13,13 +13,15 @@ class UserController {
 
 	public async create(req: Request, res: Response): Promise<Response> {
 		const { name, email, password } = req.body
+		const salt = await bcrypt.genSalt(10)
+		const hashedPassword = await bcrypt.hash(password, salt)
 
 		const createUser = new CreateUserService()
 
 		const user = await createUser.execute({
 			name,
 			email,
-			password,
+			password: hashedPassword,
 		})
 
 		return res.json(user)
